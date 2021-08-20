4a55a22f6d805c19df611f76f8acaa3d39f6e555
Michael Ripley <zkxs00@gmail.com>
2021-08-19 23:26:22 -0500

    Initial commit

---
+++ NeosTrackerIdStabilizer/NeosTrackerIdStabilizer.cs
@@ -0,0 +1,51 @@
+﻿using FrooxEngine;
+using HarmonyLib;
+using NeosModLoader;
+using System;
+using System.Reflection;
+
+namespace NeosTrackerIdStabilizer
+{
+    public class NeosTrackerIdStabilizer : NeosMod
+    {
+        public override string Name => "NeosTrackerIdStabilizer";
+        public override string Author => "runtime";
+        public override string Version => "1.0.0";
+        public override string Link => "https://github.com/zkxs/NeosTrackerIdStabilizer";
+
+        public override void OnEngineInit()
+        {
+            Harmony harmony = new Harmony("net.michaelripley.NeosTrackerIdStabilizer");
+
+            MethodInfo originalMethod = AccessTools.DeclaredPropertySetter(typeof(ViveTracker), nameof(ViveTracker.UniqueID));
+            if (originalMethod == null)
+            {
+                Error($"Could not find ViveTracker.UniqueID setter");
+                return;
+            }
+
+            MethodInfo patch = AccessTools.DeclaredMethod(typeof(NeosTrackerIdStabilizer), nameof(NeosTrackerIdStabilizer.UniqueIDSetterPostfix));
+            harmony.Patch(originalMethod, postfix: new HarmonyMethod(patch));
+            Msg("patch installed successfully");
+        }
+
+        public static void UniqueIDSetterPostfix(ViveTracker __instance, string value)
+        {
+            MethodInfo publicIdSetter = AccessTools.DeclaredPropertySetter(typeof(ViveTracker), nameof(ViveTracker.PublicID));
+            if (publicIdSetter == null)
+            {
+                Error("Could not find ViveTracker.PublicID setter");
+                return;
+            }
+            try
+            {
+                Debug($"renaming vive tracker from {__instance.PublicID} to {value}");
+                publicIdSetter.Invoke(__instance, new object[] { value });
+            }
+            catch (Exception e)
+            {
+                Error($"Exception calling ViveTracker.PublicID setter:\n{e}");
+            }
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,15 @@
+# NeosTrackerIdStabilizer
+
+A [NeosModLoader](https://github.com/zkxs/NeosModLoader) mod for [Neos VR](https://neos.com/) that restores your Vive tracker names to their old LHR-whatever IDs. This allows you to make LogiX using trackers that won't break if your local database is reset.
+
+Note that this has minor privacy implications, in that if you were to fake your own death and your trackers continued being used in Neos your elaborate ruse could be revealed.
+
+## Installation
+1. Install [NeosModLoader](https://github.com/zkxs/NeosModLoader).
+1. Place [NeosTrackerIdStabilizer.dll](https://github.com/zkxs/NeosTrackerIdStabilizer/releases/latest/download/NeosTrackerIdStabilizer.dll) into your `nml_mods` folder. This folder should be at `C:\Program Files (x86)\Steam\steamapps\common\NeosVR\nml_mods` for a default install. You can create it if it's missing, or if you launch the game once with NeosModLoader installed it will create the folder for you.
+1. Start the game. If you want to verify that the mod is working you can check your Neos logs.
+
+## FAQ
+
+### Is this against guidelines?
+Maybe. [Read more](https://github.com/zkxs/NeosModLoader/blob/master/doc/neos_guidelines.md).
